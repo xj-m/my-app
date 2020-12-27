af82a4ade790a1186384de16c21704a93a2b6a17
xj-m <xj-m@hotmail.com>
2020-12-26 19:32:49 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,209 @@
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+
+const testBorderStyle = {
+    // Tip: you can uncomment the following lines to show component borders
+    // borderStyle: "dotted",
+    // borderWidth: 3,
+};
+
+function QaScreen() {
+    // TODO: add missing components and adjust styles
+    // Tip: you can uncomment "testBorderStyle", then the border of the flexboxes will show up
+    // react native style: https://reactnative.dev/docs/style
+    // react native flexbox: https://reactnative.dev/docs/flexbox
+    return (
+        <View style={{ ...testBorderStyle, flex: 1 }}>
+            <View style={{ ...testBorderStyle, flex: 1 }} />
+            <View style={{ ...testBorderStyle, flex: 1, flexDirection: 'row' }}>
+                <View style={{
+                    ...testBorderStyle,
+                    flex: 3,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <Text style={{
+                        fontSize: 25,
+                        color: "red",
+                        fontWeight: "bold"
+                    }}>Answer Your First Question</Text>
+                </View>
+                <View style={{
+                    ...testBorderStyle,
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <AntDesign name="right" size={24} color="black" />
+                </View>
+            </View>
+            <View style={{ ...testBorderStyle, flex: 1 }}>
+                <Text>Answer Your Second Question</Text>
+            </View>
+            <View style={{ ...testBorderStyle, flex: 1 }}>
+                <Text>Answer Your Third Question</Text>
+            </View>
+            <View style={{
+                ...testBorderStyle,
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <TouchableOpacity style={{
+                    ...testBorderStyle,
+                    height: 100,
+                    width: 100,
+                    borderRadius: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "blue"
+                }}
+                    onPress={() => alert("pressed")}>
+                    <Text style={{
+                        fontSize: 25,
+                        color: "red",
+                        fontWeight: "bold"
+                    }}>Done</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+
+    );
+}
+
+function CounterScreen() {
+    // TODO: create a counter using react hook (https://reactjs.org/docs/hooks-state.html)
+    // the counter means every time you click "click me", the text number above it will increase by 1
+    const count = 0
+    return (
+        <View style={{
+            flex: 1,
+            alignContent: "center",
+            justifyContent: "center",
+        }}
+        >
+            <Text> You clicked {count} times </Text>
+            <TouchableOpacity onPress={() => alert("pressed")}>
+                <Text style={{ color: "red" }}>
+                    Click me
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+function TagScreen() {
+    // TODO: write down your idea on how to built the screen so that user can add, select or deselect tags
+    const TagBtn1 = ({ content }) => {
+        return (
+            <TouchableOpacity style={{
+                borderRadius: 15,
+                padding: 6,
+                paddingHorizontal: 5,
+                marginHorizontal: 4,
+                marginVertical: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#4069D9",
+                shadowColor: "black",
+                shadowOffset: { height: 2, width: 2 },
+                shadowOpacity: 0.4,
+            }}
+                onPress={() => { alert("pressed") }}
+            >
+                <Text style={{ fontSize: 30, color: "white" }}>{content}</Text>
+            </TouchableOpacity>
+        )
+    }
+    const TagBtn2 = ({ content }) => {
+        return (
+            <TouchableOpacity style={{
+                borderRadius: 15,
+                padding: 6,
+                paddingHorizontal: 5,
+                marginHorizontal: 4,
+                marginVertical: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                shadowColor: "black",
+                shadowOffset: { height: 2, width: 2 },
+                shadowOpacity: 0.4,
+            }}
+                onPress={() => { alert("pressed") }}
+            >
+                <Text style={{ fontSize: 30, color: "grey" }}>{content}</Text>
+            </TouchableOpacity>
+        )
+    }
+    const TagBtn3 = ({ content }) => {
+        return (
+            <TouchableOpacity style={{
+                borderRadius: 15,
+                borderStyle: "solid",
+                borderColor: "#4069D9",
+                borderWidth: 3,
+                padding: 6,
+                paddingHorizontal: 5,
+                marginHorizontal: 4,
+                marginVertical: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                shadowColor: "black",
+                shadowOffset: { height: 2, width: 2 },
+                shadowOpacity: 0.4,
+            }}
+                onPress={() => { alert("pressed") }}
+            >
+                <Text style={{ fontSize: 30, color: "grey" }}>{content}</Text>
+            </TouchableOpacity>
+        )
+    }
+    return (
+        <View style={{ ...testBorderStyle, flex: 1 }}>
+            <View style={{ ...testBorderStyle, flex: 2 }} />
+            <View style={{ ...testBorderStyle, flex: 6, paddingHorizontal: 50 }} >
+                <View style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <Text style={{
+                        fontSize: 30,
+                        color: "#122F77",
+                        fontWeight: "bold"
+                    }}>I'm interested in ...</Text>
+                </View>
+                <View style={{ ...testBorderStyle, flexDirection: "row", flexWrap: "wrap" }} >
+                    <TagBtn1 content={"#Add New"} />
+                    <TagBtn2 content={"#UI/UX"} />
+                    <TagBtn2 content={"#Disney"} />
+                    <TagBtn3 content={"#Disney"} />
+                </View>
+            </View>
+        </View>
+    );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="QA" component={QaScreen} />
+            <Tab.Screen name="Counter" component={CounterScreen} />
+            <Tab.Screen name="Tag" component={TagScreen} />
+        </Tab.Navigator>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend-expo-app
+
+## Install
+
+```shell
+# Inside the "my-app" directory
+npm install --global expo-cli
+npm install
+expo start
+# then the instructions would show up in your terminal:
+#  To run the app with live reloading, choose one of:
+#  › Scan the QR code above with the Expo app (Android) or the Camera app (iOS).
+#  › Press a for Android emulator, or i for iOS simulator, or w to run on web.
+#  › Press e to send a link to your phone with email.
+```
+
+I recommend you to download "Expo app" then scan the QR code using your phone
+
+If there's any problem, please refer to <https://docs.expo.io/get-started/installation/>
+
+## Tasks
+
+- You only need to change the code in "App.js" to finish all the tasks.
+- There are three screens inside `App.js`, `QaScreen`,`CounterScreen` and `TagScreen`, currently after you launch simulator, three screens should be like this (you can switch screens by clicking the bottom tab icons)
+
+![](images/2020-12-26-18-53-29.png)
+
+### Task1: QaScreen
+
+In `QaScreen()`, add missing components and adjust styles to make it looks like the following image
+
+![](images/2020-12-26-19-01-09.png)
+
+### Task2: CounterScreen
+
+create a counter using react hook (<https://reactjs.org/docs/hooks-state.html>)
+This means every time you click "click me", the text number above it will increase by 1
+
+### Task3: TagScreen
+
+This screen is supposed to enable the user to choose or add their tags, which means
+
+1. After the user clicking "#Add New," a new tag box will show up
+2. The user can select or deselect any tag box, and the target tag box's style will change accordingly.
+
+You don't have to change the code in `TagScreen().` You can write down your idea of implementing the required features.
